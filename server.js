b42a368ddc10c50672b161ab771255b4174525b8
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2016-10-12 10:33:25 -0600

    backend deploy

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 var path = require('path');
 var express = require('express');
 var cors = require('cors');
-var webpack = require('webpack');
-var config = require('./webpack.config');
-var compiler = webpack(config);
 
-var app = express();
+//var webpack = require('webpack');
+//var config = require('./webpack.config');
+//var compiler = webpack(config);
+//var app = express();
 
 var backend = express();
 
 var { blogs } = require('./data')
 
 var corsOptions = {
-  origin: 'http://localhost:8081',
+  origin: 'https://sbaidon.github.io/template-challenge/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 
+/*
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
+*/
 
 
-app.use(require('webpack-hot-middleware')(compiler));
+//app.use(require('webpack-hot-middleware')(compiler));
 
 //app.use('/static', express.static(__dirname + '/dist'));
+
+/*
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -38,6 +42,8 @@ app.listen(process.env.PORT || 8081 , (err) => {
   }
   console.log('Listening at http://localhost:8081');
 });
+
+*/
 
 backend.get('/blog' , cors(corsOptions), (req, res) => {
   return res.json(blogs);
