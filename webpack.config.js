162b8cfb3b822425b69984efa23bd1dae4188ed8
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2016-10-12 00:33:46 -0600

    absolute path for images

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/template-challenge/dist/'
   },
   resolve: {
       fallback: path.join(__dirname, "client", "templates", "helpers")
