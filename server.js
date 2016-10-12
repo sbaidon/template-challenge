7088804d14162cb19208db0d1e8d6f7c7a1983f1
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2016-10-12 10:43:36 -0600

    changed origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var backend = express();
 var blogs = require('./data').blogs
 
 var corsOptions = {
-  origin: 'https://sbaidon.github.io/template-challenge/',
+  origin: 'https://sbaidon.github.io',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
